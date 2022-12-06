c3b95471e5c44ddf635e1305f7cd2d86d0e0ff6d
Loki Skylizard <lokiskylizard@Lokis-MacBook-Pro.local>
2022-12-06 09:17:24 -0600

    debug script explore

--- Lambda/json_debug.py
+++ Lambda/json_debug.py
@@ -10,8 +10,8 @@ logger.setLevel(logging.DEBUG)
 
 def dispatch(event):
     # TODO implement
-    slots= event['slots'];
-    drink,qty,unit= slots['Drink'], slots['Quantity'], slots['Unit']
+    slots= event["slots"];
+    drink,qty,unit= slots["Drink"], slots["Quantity"], slots["Unit"]
     retStr= "your order of "+qty+" "+unit+ " of "+drink+ " is coming right up!";
     return {"dialogAction": {
         "type": "Close",
@@ -29,19 +29,75 @@ def lambda_handler(event, context):
     return response
 
 
+
     
+
     
     ### Intents Dispatcher ###
 def dispatch(intent_request):
-    slots= intent_request['slots'];
     """
     Called when the user specifies an intent for this bot.
     """
+    
+    slots= intent_request['slots'];
+    first_name, age, investment_amount, risk_level = slots['firstName'], slots['age'], slots['investmentAmount'], slots['riskLevel']
 
-    intent_name = intent_request["currentIntent"]["name"]
+    intent_name = intent_request['currentIntent']['name']
 
     # Dispatch to bot's intent handlers
     if intent_name == 'recommendPortfolio':
         return recommend_portfolio(intent_request)
 
     raise Exception('Intent with name ' + intent_name + ' not supported')
+
+    
+
+def lambda_handler(intent_request, context):
+    logger.debug('event={}'.format(event))
+    response = dispatch(intent_request)
+    logger.debug(response)
+    return response
+
+    
+    
+    
+    
+    
+    
+____________________________________
+
+    
+    
+    
+import json
+import logging
+
+### Intents Dispatcher ###
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+    
+    slots= intent_request['slots'];
+    first_name, age, investment_amount, risk_level = slots['firstName'], slots['age'], slots['investmentAmount'], slots['riskLevel']
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to bot's intent handlers
+    if intent_name == 'recommendPortfolio':
+        return recommend_portfolio(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+    
+
+def lambda_handler(intent_request, context):
+    logger.debug('event={}'.format(event))
+    response = dispatch(intent_request)
+    logger.debug(response)
+    return response
+    
+    
+
+
+    
